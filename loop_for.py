1d415702bb369009c0c5453641d5ec444fa3f91a
hyokyun <gyrbs22@gmail.com>
2021-05-07 21:22:51 +0900

    Update loop_for.py

--- loop_for.py
+++ loop_for.py
@@ -1,7 +1,23 @@
 N = 3
-values = [[0]*N]*N
+values = [[0] * N]* N
 
-
+# range(반복횟수, 인덱스는 0부터 시작)
 for i in range(N):
     values[i][0] = 1
 print(values)
+
+# range안에는 정수가 들어가야하므로 len(list) len(array)
+for i in range(len(values)):
+    print(i)
+
+# list내 값들을 탐색하고 싶을때는 in list 형식
+# 말 그대로 list내 value들을 모두 불러온다
+# 이중배열의 경우 배열 자체가 값들이 됨
+for i in values:
+    print(i)
+
+# 시작인덱스를 정할때
+# 시작인덱스 ~ N-1
+for i in range(1,N):
+    print(i)
+
