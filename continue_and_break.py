d65def9981dffc790ec7623e39af9732128575db
hyokyun <gyrbs22@gmail.com>
2021-05-07 21:22:39 +0900

    Create continue_and_break.py

---
+++ continue_and_break.py
@@ -0,0 +1,17 @@
+#while - continue
+i = 0
+while i < 5:
+    print(i)
+    continue
+    print(i)
+    i = i + 1
+
+
+for i in range(5):
+    for j in range(5):
+        if i < 3:
+            print(j, i)
+        else :
+            break
+
+
